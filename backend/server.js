c67db258c21d07a0cd9979eccb8013a58a054d6e
liamxerin <mbackup677@gmail.com>
2024-08-03 11:47:22 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const dotenv = require("dotenv")
 const authRoutes = require('./routes/authRoute.js')
 const messageRoutes = require('./routes/messageRoute.js')
 const userRoutes = require('./routes/userRoute.js')
+const path = require('path');
+
+
 
 
 const connectMongo = require('./db/connectMongo.js')
@@ -14,10 +17,11 @@ const { app, server } = require("./socket/socket.js")
 
 const PORT = process.env.PORT || 5000
 
+
+
 dotenv.config()
 
 //middleware
-
 
 
 app.use(express.json()) //to parse the incoming requests with payload(from req.body)
@@ -25,6 +29,10 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, ()=>{
     connectMongo()
     console.log(`Server is running ${PORT}`)
